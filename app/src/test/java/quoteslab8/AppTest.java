1fd7a34851532764b271a17876093ca06692adad
maiada-ibrahim <maiada.ibrahim.27@icloud.com>
2021-10-24 01:36:00 +0300

    DIDTEST

--- app/src/test/java/quoteslab8/AppTest.java
+++ app/src/test/java/quoteslab8/AppTest.java
@@ -35,14 +35,14 @@ class AppTest {
 //        assertEquals(ex,App convert[radnomQuote].toString());
 ////        System.out.println("Name of Author: "+convert[radnomQuote].getAuthor());
 ////        System.out.println("The Quote : " + convert[radnomQuote].getText());
-        
 
 
 
-
-    }
 
 
     }
+
+
+    
 
 }
